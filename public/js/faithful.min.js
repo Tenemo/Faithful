84d34552ac7a7ab24af84b957f10a004a15929e0
Tenemo <tenemo@gmail.com>
2017-07-05 07:00:50 +0200

    major cleanup, therapy and pet sitting added

--- public/js/faithful.min.js
+++ public/js/faithful.min.js
@@ -222,15 +222,15 @@ $(document).ready(function() {
 
     // Initialize and Configure Scroll Reveal Animation
     window.sr = ScrollReveal();
-    sr.reveal('.sr-icons', {
-        duration: 600,
-        scale: 0.3,
-        distance: '0px'
-    }, 200);
-    sr.reveal('.sr-button', {
-        duration: 1000,
-        delay: 200
-    });
+    // sr.reveal('.sr-icons', {
+    //     duration: 600,
+    //     scale: 0.3,
+    //     distance: '0px'
+    // }, 200);
+    // sr.reveal('.sr-button', {
+    //     duration: 1000,
+    //     delay: 200
+    // });
     sr.reveal('.sr-contact', {
         duration: 600,
         scale: 0.3,
