90cd4e948976877ca48b59cfc21fdbc0e65eff22
Tenemo <tenemo@gmail.com>
2017-12-04 06:13:48 +0100

    contact form html part

--- dev/contact-form/contact-2.php
+++ dev/contact-form/contact-2.php
@@ -10,11 +10,11 @@ require 'PHPMailer-master/PHPMailerAutoload.php';
 */
 
 // an email address that will be in the From field of the email.
-$fromEmail = 'demo@domain.com';
+$fromEmail = 'form@wiernypies.pl';
 $fromName = 'Demo contact form';
 
 // an email address that will receive the email with the output of the form
-$sendToEmail = 'demo@domain.com';
+$sendToEmail = 'tenemo@gmail.com';
 $sendToName = 'Demo contact form';
 
 // subject of the email
